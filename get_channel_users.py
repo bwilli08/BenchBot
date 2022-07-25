584581068d46a25eaab916bff7c8154c5d6bf598
Brent Williams <brent.williams333@gmail.com>
2022-07-24 21:08:58 -0700

    Minimum viable product - fully static users and channel info

---
+++ get_channel_users.py
@@ -0,0 +1,45 @@
+import argparse
+import json
+import logging
+
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# Read input from command line
+parser = argparse.ArgumentParser(description='Output the users in a channel')
+parser.add_argument('-c', '--credentials', required=True,
+        help='Filename of the credentials file containing Slack credentials')
+
+args = parser.parse_args()
+
+# Helper method(s)
+def readJSONFile(filename):
+    with open(filename, 'r') as f:
+        return json.loads(f.read())
+
+# Credentials loading from local file
+'''
+{
+  "SLACK_BOT_TOKEN": string, # starts with xoxb
+  "SLACK_APP_TOKEN": string, # starts with xapp
+  "SLACK_SIGNING_SECRET": string
+}
+'''
+credentials_file = args.credentials
+SLACK_CREDENTIALS = readJSONFile(credentials_file)
+SLACK_BOT_TOKEN = SLACK_CREDENTIALS['SLACK_BOT_TOKEN']
+SLACK_APP_TOKEN = SLACK_CREDENTIALS['SLACK_APP_TOKEN']
+SLACK_SIGNING_SECRET = SLACK_CREDENTIALS['SLACK_SIGNING_SECRET']
+
+# Initializes the web client with your bot token
+client = WebClient(token=SLACK_BOT_TOKEN)
+logger = logging.getLogger(__name__)
+
+try:
+    users_list = client.users_list()
+    for member in users_list['members']:
+        if not member['deleted']:
+            print((member['id'], member['name'], member['profile']['display_name']))
+except SlackApiError as e:
+    print(f"Error: {e}")
+
--- notify_slack.py
+++ notify_slack.py
@@ -1,35 +1,58 @@
-#!/usr/bin/python3
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+import argparse
+import json
+import logging
+import pytz
+import sys
 
 from datetime import datetime
-import json
-import time
-import requests
+from pytz import timezone
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
-# Idea of the Slack Bot:
-# Step One: Find all of the fixtures and times. Save these somewhere (locally/S3)
-#           Fixture object is just {home, away, utc_time}
-# Step Two: Create a local mapping of {team name -> person} i.e. Liverpool: Brent
-# Step Three: Set up Slack bot/notification
-# Step Four: Once a day, loop over the fixtures again to verify accuracy.
-# Step Five: Once a day, look at the fixture list and notify the slack bot of matchups happening tomorrow
-# Example message:
-#   Tomorrow's matchups!
-#    - 7 a.m. : Wolverhampton Wanderers (@Cameron) vs. AFC. Bournemouth (@Brandan)
+# Read input from command line
+parser = argparse.ArgumentParser(description='Send a match notifications message to a channel.')
+parser.add_argument('-c', '--credentials', required=True,
+        help='Filename of the credentials file to use to send a message')
+parser.add_argument('-ch', '--channel', required=True,
+        help='Slack channel ID to post the message to')
+parser.add_argument('-p', '--people', required=True,
+        help='Filename of the club-to-person mappings we care about')
 
+args = parser.parse_args()
+
+# Helper method(s)
+def readJSONFile(filename):
+    with open(filename, 'r') as f:
+        return json.loads(f.read())
+
+# Credentials loading from local file
+'''
+{
+  "SLACK_BOT_TOKEN": string, # starts with xoxb
+  "SLACK_APP_TOKEN": string, # starts with xapp
+  "SLACK_SIGNING_SECRET": string
+}
+'''
+credentials_file = args.credentials
+SLACK_CREDENTIALS = readJSONFile(credentials_file)
+SLACK_BOT_TOKEN = SLACK_CREDENTIALS['SLACK_BOT_TOKEN']
+SLACK_APP_TOKEN = SLACK_CREDENTIALS['SLACK_APP_TOKEN']
+SLACK_SIGNING_SECRET = SLACK_CREDENTIALS['SLACK_SIGNING_SECRET']
+
+# Get the club-to-userId mappings, user ids from Slack users API (see get_channel_users.py script)
+'''
 club_to_person = {
-        'Tottenham': "@nateee",
-        'Leeds': "@cbotcritter",
-        'Wolves': "@ckoizumil",
-        'Bournemouth': "@brandan",
-        'M. United': "@al",
-        'Southampton': "@jonnyrottin",
-        'Newcastle': "Marcus",
-        'Crystal Palace': "@Zach",
-        'Nottingham Forest': "@kwong",
-        'Liverpool': "@thealpacca"
+    'Tottenham': "USER_ID_1",
+    'Leeds': "USER_ID_2",
+    ...
 }
 
+A club being absent in the mapping means we don't care about that club
+'''
+mapping_file = args.people
+club_to_person = readJSONFile(mapping_file)
+
+# Fixture information
 ''' Fixture object
 {
   "home": "Fulham",
@@ -37,30 +60,50 @@ club_to_person = {
   "datetime_utc": "2023-03-11T15:00:00+00:00"
 }
 '''
-fixture_filename = "./fixtures.json"
-with open(fixture_filename, 'r') as fixture_file:
-    all_fixtures = json.loads(fixture_file.read())
+all_fixtures = readJSONFile('./fixtures.json')
 
 
-#today = datetime.utcnow().date()
-today = datetime.strptime("2022-08-05T10:00:00", "%Y-%m-%dT%H:%M:%S").date()
+today = datetime.utcnow().date()
 
-games_to_notify = []
+# Compute what fixtures we need to even notify about
+fixture_notifications = []
 for fixture in all_fixtures:
     # Is this a match up we even care about?
     home_team = fixture['home']
     away_team = fixture['away']
     if home_team in club_to_person and away_team in club_to_person:
-        print("This is a match up! {} vs {}".format(club_to_person[home_team], club_to_person[away_team]))
-
         fixture_time = fixture['datetime_utc']
-        fixture_date = datetime.strptime(fixture_time, "%Y-%m-%dT%H:%M:%S").date()
+        fixture_datetime = datetime.strptime(fixture_time + " +0000", "%Y-%m-%dT%H:%M:%S %z")
+        fixture_date = fixture_datetime.date()
 
         days_until_game = (fixture_date - today).days
-
         # Game is tomorrow
         if days_until_game == 1:
-            print("Game is tomorrow, game should be included in Slack Bot message")
+            fixture['datetime_utc'] = fixture_datetime
+            fixture_notifications.append(fixture)
 
+# Build the notification message
+message = "Test notification"
+#message = "There are some match ups tomorrow!"
+for fixture in fixture_notifications:
+    message = message + "\n"
 
+    fixture_datetime = fixture['datetime_utc']
+    in_pst = fixture_datetime.astimezone(timezone('US/Pacific'))
+    friendly_time = in_pst.strftime("%I%p").lower()
+
+    home = fixture['home']
+    away = fixture['away']
+    line = "  {} - {} (<@{}>) vs. {} (<@{}>)".format(friendly_time, home, club_to_person[home], away, club_to_person[away])
+    message = message + line
+
+# Initializes the web client with your bot token
+client = WebClient(token=SLACK_BOT_TOKEN)
+logger = logging.getLogger(__name__)
+
+channel_id = args.channel
+try:
+    result = client.chat_postMessage(channel=channel_id, text=message)
+except SlackApiError as e:
+    print(f"Error: {e}")
 
