e68b1f1a7528eeaded5b1d9efda876ed15461de2
Brent Williams <brent.williams333@gmail.com>
2022-08-13 04:21:02 +0000

    Lowercase weekday name to work around cron

--- scripts/schedule_jobs.sh
+++ scripts/schedule_jobs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # We want to run at 5pm PST on Thursdays -> 12am UTC on Fridays
-echo "0 0 * * FRI ubuntu /bin/python3 /home/ubuntu/PremierLeagueMatchBot/scripts/scrape_data.py -f /home/ubuntu/PremierLeagueMatchBot/data/fixtures.json &> /home/ubuntu/PremierLeagueMatchBot/fixtures-\$(date +%s).log" > /etc/cron.d/update_fixtures
-echo "0 0 * * FRI ubuntu /bin/python3 /home/ubuntu/PremierLeagueMatchBot/scripts/notify_slack.py -c .credentials/cavilist_channel_credentials -ch C0MKGPY1K -p .users/cavilist.json -f /home/ubuntu/PremierLeagueMatchBot/data/fixtures.json &> /home/ubuntu/PremierLeagueMatchBot/notify-\$(date +%s).log" > /etc/cron.d/notify_slack
+echo "0 0 * * fri ubuntu /bin/python3 /home/ubuntu/PremierLeagueMatchBot/scripts/scrape_data.py -f /home/ubuntu/PremierLeagueMatchBot/data/fixtures.json &> /home/ubuntu/PremierLeagueMatchBot/fixtures-\$(date +%s).log" > /etc/cron.d/update_fixtures
+echo "0 0 * * fri ubuntu /bin/python3 /home/ubuntu/PremierLeagueMatchBot/scripts/notify_slack.py -c /home/ubuntu/PremierLeagueMatchBot/.credentials/cavilist_channel_credentials -ch C0MKGPY1K -p /home/ubuntu/PremierLeagueMatchBot/.users/cavilist.json -f /home/ubuntu/PremierLeagueMatchBot/data/fixtures.json &> /home/ubuntu/PremierLeagueMatchBot/notify-\$(date +%s).log" > /etc/cron.d/notify_slack
